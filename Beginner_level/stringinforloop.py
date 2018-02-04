10953f9cfe18c865d743484415003fd5739a5d8f
SRIDEVI98 <36121241+SRIDEVI98@users.noreply.github.com>
2018-02-04 15:43:02 +0530

    Add files via upload

---
+++ Beginner_level/stringinforloop.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      student
+#
+# Created:     04/02/2018
+# Copyright:   (c) student 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+string = "Hello World"
+for x in string:
+    print (x)
