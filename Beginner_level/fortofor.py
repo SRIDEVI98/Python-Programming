8468d6069f94212c00aefadcc5c8c7be437bc493
SRIDEVI98 <36121241+SRIDEVI98@users.noreply.github.com>
2018-02-04 15:39:19 +0530

    Add files via upload

---
+++ Beginner_level/fortofor.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      student
+#
+# Created:     04/02/2018
+# Copyright:   (c) student 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+list = [ [1, 2, 3], [4, 5, 6], [7, 8, 9]]
+for l in list:
+ for x in l:
+   print(x)
