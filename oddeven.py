347cb1ed9f6b911adb9927886c495bf43114732e
SRIDEVI98 <36121241+SRIDEVI98@users.noreply.github.com>
2018-02-04 16:13:11 +0530

    Add files via upload

---
+++ oddeven.py
@@ -0,0 +1,22 @@
+#-------------------------------------------------------------------------------
+# Name:        module2
+# Purpose:
+#
+# Author:      student
+#
+# Created:     04/02/2018
+# Copyright:   (c) student 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+print"Odd number:"
+for x in range(1,20,2):
+ print(x)
+print"Even number:"
+for y in range(0,20,2):
+ print(y)
