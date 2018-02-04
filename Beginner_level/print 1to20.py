2ee5bf3b02efba95e64fc6cee6e2d09c16c28c38
SRIDEVI98 <36121241+SRIDEVI98@users.noreply.github.com>
2018-02-04 15:42:27 +0530

    Add files via upload

---
+++ Beginner_level/print 1to20.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# Name:        module2
+# Purpose:
+#
+# Author:      student
+#
+# Created:     04/02/2018
+# Copyright:   (c) student 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+for x in range(1,21):
+    print(x)
+
