188f3741e632137b31f81ba6b46990017e2fb3c3
SRIDEVI98 <36121241+SRIDEVI98@users.noreply.github.com>
2018-02-04 15:39:52 +0530

    Add files via upload

---
+++ Beginner_level/forwithinsquare.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      student
+#
+# Created:     04/02/2018
+# Copyright:   (c) student 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+square=[x**2 for x in range(1,11)]
+print(square)
