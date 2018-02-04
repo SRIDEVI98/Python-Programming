9bc3ea9f1526dedfdf7fc8ce77374019d35e1caa
SRIDEVI98 <36121241+SRIDEVI98@users.noreply.github.com>
2018-02-04 15:36:01 +0530

    Add files via upload

---
+++ Beginner_level/list.py
@@ -0,0 +1,25 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Administrator
+#
+# Created:     04/02/2018
+# Copyright:   (c) Administrator 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+bikes=['trek','redline','giant']
+firstbike=bikes[0]
+secondbike=bikes[1]
+lastbike=bikes[2]
+print(firstbike)
+print(secondbike)
+print(lastbike)
+total_bike = firstbike +' '+ secondbike+' ' + lastbike
+print(total_bike)
