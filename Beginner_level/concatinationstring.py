790862bb2ff936e407d82c1368071492236deae7
SRIDEVI98 <36121241+SRIDEVI98@users.noreply.github.com>
2018-02-04 15:28:57 +0530

    Add files via upload

---
+++ Beginner_level/concatinationstring.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Administrator
+#
+# Created:     04/02/2018
+# Copyright:   (c) Administrator 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+firstname='Albert'
+lastname='einstein'
+fullname=firstname+' '+lastname
+print(fullname)
