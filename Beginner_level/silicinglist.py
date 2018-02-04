3c3d85248f22e218133650a69181abdd9f324601
SRIDEVI98 <36121241+SRIDEVI98@users.noreply.github.com>
2018-02-04 15:41:09 +0530

    Add files via upload

---
+++ Beginner_level/silicinglist.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      student
+#
+# Created:     04/02/2018
+# Copyright:   (c) student 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+finishers=['sam','bob','ada','bea']
+first2=finishers[:2]
+print(first2)
