5758bb86c7e754e33e1018e25468a98407094017
SRIDEVI98 <36121241+SRIDEVI98@users.noreply.github.com>
2018-02-04 15:36:39 +0530

    Add files via upload

---
+++ Beginner_level/forloop.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Administrator
+#
+# Created:     04/02/2018
+# Copyright:   (c) Administrator 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+colours=['red','green','blue']
+for colour in colours:
+    print(colour)
