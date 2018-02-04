21472e78266c8db8748086ada5eecfa9e5460ed8
SRIDEVI98 <36121241+SRIDEVI98@users.noreply.github.com>
2018-02-04 15:40:26 +0530

    Add files via upload

---
+++ Beginner_level/addlist.py
@@ -0,0 +1,24 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      student
+#
+# Created:     04/02/2018
+# Copyright:   (c) student 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+vehicle=['  Bycycle','  Bike','  Car']
+vehicle.append('  Bus')
+vehicle.append('  Train')
+vehicle.append('  Aeroplan')
+print"vehicles:"
+for v in vehicle:
+ print(v)
+
