5263c2dc5cd088acfa404a215b422e63678b69c6
SRIDEVI98 <36121241+SRIDEVI98@users.noreply.github.com>
2018-02-11 18:46:00 +0530

    Add files via upload

---
+++ Beginner_level/arithmatic.py
@@ -0,0 +1,17 @@
+a=10
+b=15
+c=100
+d=700
+e=40
+f=150
+g=50
+h=600
+i=4
+j=5
+sum=a+b
+mul=g*i
+div=d/c
+print("The sum of {0} and {1} is:{2}".format(a,b,sum))
+print("The multiplication of {0} and {1} is :{2}".format(g,i,mul))
+print("The division of {0} and {1} is :{2}".format(d,c,div))
+
