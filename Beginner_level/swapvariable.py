cd66ba3c25b470fd74b93e2a8c4a6e4ee6d79b3b
SRIDEVI98 <36121241+SRIDEVI98@users.noreply.github.com>
2018-02-12 21:27:25 +0530

    Add files via upload

---
+++ Beginner_level/swapvariable.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      LARATWINS
+#
+# Created:     12/02/2018
+# Copyright:   (c) LARATWINS 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+x = 3
+y = 5
+x,y =y,x
+print"x=",x
+print"y=",y
