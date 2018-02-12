c4f90e09ceeb73c1b02f09c2df6a870415af3612
SRIDEVI98 <36121241+SRIDEVI98@users.noreply.github.com>
2018-02-12 21:20:39 +0530

    Add files via upload

---
+++ Beginner_level/str and int.py
@@ -0,0 +1,25 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      LARATWINS
+#
+# Created:     12/02/2018
+# Copyright:   (c) LARATWINS 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
+d="3"
+n=int(d)+1
+print(n)
+n=d+"1"
+print(n)
+
+
+
