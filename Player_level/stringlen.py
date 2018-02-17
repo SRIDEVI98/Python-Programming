e4bc25c06640f5f2d2477f0e163541862b1cb741
SRIDEVI98 <36121241+SRIDEVI98@users.noreply.github.com>
2018-02-17 12:45:16 +0530

    Add files via upload

---
+++ Player_level/stringlen.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      LARATWINS
+#
+# Created:     17/02/2018
+# Copyright:   (c) LARATWINS 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+x = {1:'a', 2:'b', 3:'c'}
+y = {}
+print(len(x))
+print(len(y))
