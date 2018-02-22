c034a3cfca04a49bac4476eddb4dd8915b151d64
SRIDEVI98 <36121241+SRIDEVI98@users.noreply.github.com>
2018-02-22 07:53:24 +0530

    Add files via upload

---
+++ Player_level/area&circumof circle.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      LARATWINS
+#
+# Created:     21/02/2018
+# Copyright:   (c) LARATWINS 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+r=float(input("radius:"))
+area=3.14*r*r
+circumference=2*3.14*r
+print"The area of circle is:",area
+print"The circumference of circle is:",circumference
