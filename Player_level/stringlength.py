ba4b32c2cdf912347821ffd42415d24dd91f101e
SRIDEVI98 <36121241+SRIDEVI98@users.noreply.github.com>
2018-02-22 22:25:24 +0530

    Add files via upload

---
+++ Player_level/stringlength.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      LARATWINS
+#
+# Created:     22/02/2018
+# Copyright:   (c) LARATWINS 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+string='programmer'
+s=len(string)
+print"The length of the string is:",s
