eac7a8940d256c4a7c741c70bf4fca3e71828da4
SRIDEVI98 <36121241+SRIDEVI98@users.noreply.github.com>
2018-02-22 07:21:50 +0530

    Add files via upload

---
+++ Player_level/mathmaticalopration.py
@@ -0,0 +1,35 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      LARATWINS
+#
+# Created:     21/02/2018
+# Copyright:   (c) LARATWINS 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+a=5
+b=6
+c=-4
+d= 8
+e=4
+f=3
+g=9
+h=3
+i=10
+j= 15
+
+print("The sum of {0} and {1} is:{2}".format(a,b,a+b))
+print("The subraction of {0} and {1} is:{2}".format(c,d,c-d))
+print("The multiplication of {0} and {1} is:{2}".format(e,f,e*f))
+print("The division of {0} and {1} is:{2}".format(j,i,j/i))
+print("The modulo of {0} and {1} is:{2}".format(j,i,j%i))
+print("The exponantial of {0} and {1} is:{2}".format(a,f,a**f))
+print("The floor division of {0} and {1} is:{2}".format(e,f,e//f))
+print("The floor division of {0} and {1} is:{2}".format(c,f,c//f))
