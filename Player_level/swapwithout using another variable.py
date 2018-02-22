b390a7e4264d6fd2e3340881e0247be504e0ac83
SRIDEVI98 <36121241+SRIDEVI98@users.noreply.github.com>
2018-02-22 07:54:56 +0530

    Add files via upload

---
+++ Player_level/swapwithout using another variable.py
@@ -0,0 +1,23 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      LARATWINS
+#
+# Created:     21/02/2018
+# Copyright:   (c) LARATWINS 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+a=5
+b=6
+a=a+b
+b=a-b
+a=a-b
+print"a:",a
+print"b:",b
