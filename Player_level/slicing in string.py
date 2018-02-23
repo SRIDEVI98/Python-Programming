dc35bf106304d20b20d1859816a78122448b7a12
SRIDEVI98 <36121241+SRIDEVI98@users.noreply.github.com>
2018-02-23 22:47:42 +0530

    Add files via upload

---
+++ Player_level/slicing in string.py
@@ -0,0 +1,26 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      LARATWINS
+#
+# Created:     22/02/2018
+# Copyright:   (c) LARATWINS 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+print('word'[-1])
+print('Pawel'[1:3])
+word = 'assume'
+print(word[:])
+print(word[:2])
+print(word[2:])
+print(word[1:3])
+
+
+
