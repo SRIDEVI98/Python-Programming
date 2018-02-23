fc49c1601b480b6dc8fcd4cab2cd4634de51fd95
SRIDEVI98 <36121241+SRIDEVI98@users.noreply.github.com>
2018-02-23 23:29:59 +0530

    Add files via upload

---
+++ Player_level/stringmethodassignment.py
@@ -0,0 +1,26 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      LARATWINS
+#
+# Created:     23/02/2018
+# Copyright:   (c) LARATWINS 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+string="Programmer"
+c=string.count('r')
+print(c)
+f=string.find('g')
+print(f)
+e=string.replace('m','Y')
+print(e)
+
+
+
