b72e842080fb3f1a9a9c6d6ef5284a4f2dc919b7
SRIDEVI98 <36121241+SRIDEVI98@users.noreply.github.com>
2018-02-24 00:38:24 +0530

    Add files via upload

---
+++ Player_level/age eligible for voating or not.py
@@ -0,0 +1,22 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      LARATWINS
+#
+# Created:     24/02/2018
+# Copyright:   (c) LARATWINS 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+age=int(input("Enter the age:"))
+print("Whether the age is eligible for voting or not.")
+if age==20:
+    print("eligible.")
+else:
+    print("not eligible.")
