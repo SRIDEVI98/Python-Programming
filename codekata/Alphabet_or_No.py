06a7ded2ed5c7048ce0497d53e3569f42b2b8042
SRIDEVI98 <36121241+SRIDEVI98@users.noreply.github.com>
2018-02-25 15:38:54 +0530

    Create Alphabet_or_No.py

---
+++ codekata/Alphabet_or_No.py
@@ -0,0 +1,9 @@
+small={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'}
+caps={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'}
+n=raw_input("Enter the character:")
+if n in small:
+    print("Alphabet")
+elif n in caps:
+    print("Alphabet")
+else:
+    print("No")
