aedd86daa30df6dbd3eb1db644f6d527f1f9cd5a
SRIDEVI98 <36121241+SRIDEVI98@users.noreply.github.com>
2018-02-25 14:54:21 +0530

    Update and rename vowel or consonant.py to vowel _or_ consonant.py

--- codekata/vowel _or_ consonant.py
+++ codekata/vowel _or_ consonant.py
@@ -1,6 +1,6 @@
 vowels={'a','e','i','o','u'}
 n=raw_input("Enter the character:")
 if n in vowels:
-    print("The character is vowel")
+    print("Vowel")
 else:
-    print("The character is consonant")
+    print("Consonant")
