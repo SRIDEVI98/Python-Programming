c90fb3fb8f0e7f939d780139acf4f94eb210d4b1
SRIDEVI98 <36121241+SRIDEVI98@users.noreply.github.com>
2018-03-11 13:07:49 +0530

    Update sum_of_array.py

--- codekata/sum_of_array.py
+++ codekata/sum_of_array.py
@@ -1,9 +1,9 @@
 a=[]
-n=int(input("Enter size array:"))
-k=int(input("Enter the value to add the number in given array:"))
+n=int(input(""))
+k=int(input(""))
 sum=0
 for i in range(n):
-    a.append(int(input("Enter the number:")))
+    a.append(int(input("")))
 for i in range(k):
     sum=sum+a[i]
 print(sum)
